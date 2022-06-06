421e40647b36bd58a52a72f83c91751de80c95d2
sangwonme <paulmoguri@naver.com>
2022-06-07 03:04:02 +0900

    :bug: Fix input to textarea tag

--- src/components/CVPageRelated/ExpTable/ExpTable.tsx
+++ src/components/CVPageRelated/ExpTable/ExpTable.tsx
@@ -45,24 +45,24 @@ function ExpTable() {
           </div>
         }
         <div className={styles.expName}>
-          <input className={styles.input} type="text" placeholder='활동명'/>
+          <input className={styles.input} placeholder='활동명'/>
           <p className={styles.dropdown} onClick={toggleDrop}>{keyword}{useDrop? '▲' : '▼'}</p>
         </div>
         <div className={styles.expInfo}>
           <p className={styles.label}>활동 개요</p>
-          <input className={styles.input} type="text" placeholder='활동 개요 및 문제 상황' />
+          <textarea className={styles.input} placeholder='활동 개요 및 문제 상황' />
         </div>
         <div className={styles.expInfo}>
           <p className={styles.label}>나의 노력</p>
-          <input className={styles.input} type="text" placeholder='문제를 해결한 / 상황을 개선한 나의 노력' />
+          <textarea className={styles.input} placeholder='문제를 해결한 / 상황을 개선한 나의 노력' />
         </div>
         <div className={styles.expInfo}>
           <p className={styles.label}>나의 성과</p>
-          <input className={styles.input} type="text" placeholder='문제가 해결된 / 상황이 개선된 결과' />
+          <textarea className={styles.input} placeholder='문제가 해결된 / 상황이 개선된 결과' />
         </div>
         <div className={styles.expInfo}>
           <p className={styles.label}>깨달음</p>
-          <input className={styles.lastInput} type="text" placeholder='이 활동에서 내가 얻은 깨달음' />
+          <textarea className={styles.lastInput && styles.input} placeholder='이 활동에서 내가 얻은 깨달음' />
         </div>
       </div>
     </div>
