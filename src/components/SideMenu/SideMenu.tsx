f1166ba2cc68ffaf5a056762a68dbbbd094a7ac2
sangwonme <paulmoguri@naver.com>
2022-06-06 17:57:41 +0900

    :art: Fix SideMenu Style

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -6,17 +6,19 @@ function SideMenu() {
     <>
       <div className={styles.container}>
         <p className={styles.title}>나의 지원서 관리</p>
-        <p className={styles.basicInfo}>기본 정보</p>
+        <div className={styles.category}>
+          <p className={styles.basicInfo}>기본정보</p>
+        </div>
         <div className={styles.category}>
           <p className={styles.categoryTitle}>경험</p>
-          <p className={styles.subnav}>수업 활동</p>
-          <p className={styles.subnav}>동아리·학회·대외 활동</p>
-          <p className={styles.subnav}>인턴·경력</p>
+          <p className={styles.subnav}>📚 수업활동</p>
+          <p className={styles.subnav}>🗣 동아리 / 학회 / 대외활동</p>
+          <p className={styles.subnav}>📑 인턴 및 경력</p>
         </div>
         <div className={styles.category}>
           <p className={styles.categoryTitle}>기타</p>
-          <p className={styles.subnav}>사용 가능한 스킬</p>
-          <p className={styles.subnav}>어학 성적·자격증·수상 경력</p>
+          <p className={styles.subnav}>🔎 사용 가능한 스킬</p>
+          <p className={styles.subnav}>📂 어학성적 / 자격증 / 수상경력</p>
         </div>
       </div>
     </>
--- src/pages/CVPage/CVPage.tsx
+++ src/pages/CVPage/CVPage.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import Header from '../../components/Header/Header';
+import SideMenu from '../../components/SideMenu/SideMenu';
 import styles from './CVPage.module.scss';
 
 function CVPage() {
   return (
     <div className={styles.page}>
       <Header/>
+      <div className={styles. main}>
+        <SideMenu/>
+      </div>
     </div>
   );
 }
